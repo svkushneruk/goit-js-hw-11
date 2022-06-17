de9c49bb875178a8e0681265d834497a4db17cee
Serhii Kushneruk <svkushneruk@ukr.net>
2022-06-17 20:06:31 +0300

    Додає частину додаткових завдань з д/з

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import './css/styles.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import ImageSearchService from './css/js/api';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// import throttle from 'lodash.throttle';
 
 const imageSearchService = new ImageSearchService();
 
@@ -19,53 +22,33 @@ function onSearch(e) {
   hideLoadMoreBtn();
   clearGallery();
   imageSearchService.resetPage();
-  imageSearchService.query = e.currentTarget.elements.searchQuery.value;
+  imageSearchService.query = e.currentTarget.elements.searchQuery.value.trim();
+
   if (imageSearchService.query === '') {
+    Notify.failure('Please, enter your query.');
     return;
   }
+
   imageSearchService.fetchImages().then(data => {
-    console.log(data);
     if (data.totalHits === 0) {
       Notify.failure(
         'Sorry, there are no images matching your search query. Please try again.'
       );
       return;
     }
+    Notify.info(`Hooray! We found ${data.totalHits} images.`);
 
-    const template = data.hits
-      .map(
-        ({
-          webformatURL,
-          largeImageURL,
-          tags,
-          likes,
-          views,
-          comments,
-          downloads,
-        }) => {
-          return `
-            <div class="photo-card">
-                <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-                <div class="info">
-                    <p class="info-item">
-                        <b> Likes</b> ${likes}
-                    </p>
-                    <p class="info-item">
-                        <b> Views</b> ${views}
-                    </p>
-                    <p class="info-item">
-                        <b> Comments</b> ${comments}
-                    </p>
-                    <p class="info-item">
-                        <b> Downloads</b> ${downloads}
-                    </p>
-                </div>
-            </div>
-            `;
-        }
-      )
-      .join('');
-    refs.galleryEl.insertAdjacentHTML('beforeend', template);
+    const markup = createMarkup(data.hits);
+
+    refs.galleryEl.insertAdjacentHTML('beforeend', markup);
+
+    // const { height: topScroll } = document
+    //   .querySelector('.search-section')
+    //   .getBoundingClientRect();
+    // window.scrollTo(0, topScroll);
+
+    // window.addEventListener('wheel', throttle(scrollGallery, 1000));
+    const lightbox = new SimpleLightbox('.gallery a', {});
     showLoadMoreBtn();
     if (data.hits.length > 0 && data.hits.length < 40) {
       Notify.info("We're sorry, but you've reached the end of search results.");
@@ -74,28 +57,49 @@ function onSearch(e) {
   });
 }
 
+function scrollGallery() {
+  console.log('scroll');
+
+  const { height: cardHeight } =
+    refs.galleryEl.firstElementChild.getBoundingClientRect();
+
+  console.log(cardHeight);
+
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
+}
+
 function onLoadMore() {
   imageSearchService.fetchImages().then(data => {
-    if (data.hits.length === 0) {
-      Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
-      return;
+    const markup = createMarkup(data.hits);
+    refs.galleryEl.insertAdjacentHTML('beforeend', markup);
+    const lightbox = new SimpleLightbox('.gallery a', {});
+    if (data.hits.length > 0 && data.hits.length < 40) {
+      Notify.info("We're sorry, but you've reached the end of search results.");
+      hideLoadMoreBtn();
     }
-    const template = data.hits
-      .map(
-        ({
-          webformatURL,
-          largeImageURL,
-          tags,
-          likes,
-          views,
-          comments,
-          downloads,
-        }) => {
-          return `
+  });
+}
+
+function createMarkup(data) {
+  return data
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `
             <div class="photo-card">
-                <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+                <a href="${largeImageURL}" class="big-img-link" >
+                  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+                </a>
                 <div class="info">
                     <p class="info-item">
                         <b> Likes</b> ${likes}
@@ -112,15 +116,9 @@ function onLoadMore() {
                 </div>
             </div>
             `;
-        }
-      )
-      .join('');
-    refs.galleryEl.insertAdjacentHTML('beforeend', template);
-    if (data.hits.length > 0 && data.hits.length < 40) {
-      Notify.info("We're sorry, but you've reached the end of search results.");
-      hideLoadMoreBtn();
-    }
-  });
+      }
+    )
+    .join('');
 }
 
 function showLoadMoreBtn() {
